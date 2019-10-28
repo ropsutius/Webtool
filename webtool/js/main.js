592ffe9e0da6a06dd8c33ac1cbd8e38aa9b29b1b
Roope Lehikoinen <ropsutius@gmail.com>
2019-10-28 17:14:47 +0200

    Fixed window resizing

--- webtool/js/main.js
+++ webtool/js/main.js
@@ -1,10 +1,19 @@
 let changed3D = null;
 let changedPixel = null;
 
+function onWindowResize(list) {
+  for (let i = 0; i < list.length; i++) {
+    list[i].onWindowResize();
+  }
+}
+
 window.onload = function() {
   canvasP = document.querySelector("#pixel-view");
   canvas3d = document.getElementById("3d-view");
   pv = new PixelView(canvasP);
   tv = new ThreeDView(canvas3d);
-  window.onresize = pv.onWindowResize.bind(pv);
+  window.onresize = function() {
+    pv.onWindowResize();
+    tv.onWindowResize();
+  };
 };
