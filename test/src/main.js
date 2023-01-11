a7ae99b7a5d32f7dec15c29a9c9eb3122d0afc83
ropsutius <ropsutius@gmail.com>
2023-01-11 23:10:04 +0200

    Changed README.md and name of the title

--- test/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import * as WebTool from './webtool/app.js';
-
-import './assets/main.css';
-
-createApp(App).mount('#app');
-
-WebTool.initApp();
--- test/src/webtool/services/weavesService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const baseUrl = './src/webtool/weaves/';
-
-export async function importWeaves() {
-  const response = await axios.get(baseUrl + 'plainWeave.json');
-  return response.data;
-}
